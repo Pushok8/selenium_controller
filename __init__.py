931171f10653ca8206aaf22a33e9d8d1a1b462fe
Pushok8 <pushok8@protonmail.com>
2021-07-03 17:08:24 +0300

    Added autor name and version.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
 from .selenium_controllers.css_selenium_controller import SeleniumController as CSSSeleniumController
 from .selenium_controllers.xpath_selenium_controller import SeleniumController as XPathSeleniumController
+
+
+__author__ = 'Pushok8'
+__version__ = '0.1'
